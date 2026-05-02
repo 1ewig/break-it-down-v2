19f1f845250c888e3497b8f9d9fb61aabcabf365
Moshu <sellfor59@gmail.com>
2026-05-02 20:27:17 +0500

    Cleanup: Removed TanStack Query Devtools

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
@@ -22,8 +21,6 @@ export default function QueryProvider({ children }: { children: React.ReactNode 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* Devtools are automatically excluded in production builds */}
-      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </QueryClientProvider>
   );
 }
