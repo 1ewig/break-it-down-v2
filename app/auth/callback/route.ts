c164cb50a026a378b6eca07d31f6ce7761c53bbb
Moshu <sellfor59@gmail.com>
2026-05-14 18:14:46 +0500

    create auth callback route for password reset

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,20 @@
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { createClient } from '@/lib/supabase/server'
+import { redirect } from 'next/navigation'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/home'
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
+    if (!error) {
+      redirect(type === 'recovery' ? '/update-password' : next)
+    }
+  }
+
+  redirect('/login?message=Could not verify')
+}
