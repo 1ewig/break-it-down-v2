eb226523dbbfb53d1a252212bf6118865a250ae9
Moshu <sellfor59@gmail.com>
2026-05-14 18:55:49 +0500

    also handle code param in auth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,36 +1,44 @@
-import { type EmailOtpType } from '@supabase/supabase-js'
 import { createServerClient } from '@supabase/ssr'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') as EmailOtpType | null
+  const code = searchParams.get('code')
+  const type = searchParams.get('type')
   const next = searchParams.get('next') ?? '/home'
 
   const redirectTo = request.nextUrl.clone()
   redirectTo.pathname = type === 'recovery' ? '/update-password' : next
 
-  if (token_hash && type) {
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll()
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) => {
-              request.cookies.set(name, value)
-              redirectTo.cookies.set(name, value, options)
-            })
-          },
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
         },
-      }
-    )
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            request.cookies.set(name, value)
+            redirectTo.cookies.set(name, value, options)
+          })
+        },
+      },
+    }
+  )
 
-    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
+  if (token_hash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      type: type as 'email' | 'sms' | 'recovery' | 'invite' | 'email_change',
+      token_hash,
+    })
+    if (!error) return NextResponse.redirect(redirectTo)
+  }
+
+  if (code) {
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) return NextResponse.redirect(redirectTo)
   }
 
