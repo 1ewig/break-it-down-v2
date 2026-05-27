d0066e2ee6e992779236c7e1aa02cad13091c94a
Moshu <sellfor59@gmail.com>
2026-05-27 16:35:48 +0500

    fix: cast ease array as tuple for motion Variants type

--- components/home/TaskAcknowledged.tsx
+++ components/home/TaskAcknowledged.tsx
@@ -16,7 +16,7 @@ const child = {
   animate: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] as [number, number, number, number] },
   },
 };
 
